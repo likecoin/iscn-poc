b67a413b2d4ab0d4ec3adad75815fafb8545343e
Aludirk Wong <aludirk@like.co>
2020-01-30 19:09:46 +0800

    Test IPFS as library

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/likecoin/likecoin-iscn-poc
+
+go 1.13
+
+require (
+	github.com/ipfs/go-ipfs v0.0.0-00010101000000-000000000000
+	github.com/ipfs/go-ipfs-config v0.2.0
+	github.com/ipfs/interface-go-ipfs-core v0.2.5
+)
+
+replace github.com/ipfs/go-ipfs => ./go-ipfs
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/coreapi"
+	"github.com/ipfs/go-ipfs/plugin/loader"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+
+	config "github.com/ipfs/go-ipfs-config"
+	libp2p "github.com/ipfs/go-ipfs/core/node/libp2p"
+	icore "github.com/ipfs/interface-go-ipfs-core"
+)
+
+func setupNode(ctx context.Context) (*loader.PluginLoader, icore.CoreAPI, error) {
+	rootPath, err := filepath.Abs("./ipfs")
+	if err != nil {
+		log.Printf("Cannot parse path: %s", err)
+		return nil, nil, err
+	}
+
+	plugins, err := loader.NewPluginLoader(rootPath)
+	if err != nil {
+		log.Printf("error loading plugins: %s", err)
+		return nil, nil, err
+	}
+
+	if err := plugins.Initialize(); err != nil {
+		log.Printf("error initializing plugins: %s", err)
+		return nil, nil, err
+	}
+
+	if err := plugins.Inject(); err != nil {
+		log.Printf("error initializing plugins: %s", err)
+		return nil, nil, err
+	}
+
+	rootPath, err = config.Path(rootPath, "")
+	if err != nil {
+		log.Printf("Cannot set config path: %s", err)
+		return nil, nil, err
+	}
+
+	cfg, err := config.Init(ioutil.Discard, 2048)
+	if err != nil {
+		log.Printf("Cannot init config: %s", err)
+		return nil, nil, err
+	}
+
+	err = fsrepo.Init(rootPath, cfg)
+	if err != nil {
+		log.Printf("Cannot init repo: %s", "ab")
+		return nil, nil, err
+	}
+
+	repo, err := fsrepo.Open(rootPath)
+	if err != nil {
+		log.Printf("Cannot open repo: %s", err)
+		return nil, nil, err
+	}
+
+	nodeOptions := &core.BuildCfg{
+		Online:  true,
+		Routing: libp2p.DHTOption,
+		Repo:    repo,
+	}
+
+	node, err := core.NewNode(ctx, nodeOptions)
+	if err != nil {
+		log.Printf("Cannot create node: %s", err)
+		return nil, nil, err
+	}
+	node.IsDaemon = true
+
+	log.Println("Start plugin")
+	err = plugins.Start(node)
+	if err != nil {
+		log.Printf("Cannot start plugins: %s", err)
+		return nil, nil, err
+	}
+
+	ipfs, err := coreapi.NewCoreAPI(node)
+	if err != nil {
+		log.Println("No IPFS repo available on the default path")
+		return nil, nil, err
+	}
+
+	return plugins, ipfs, nil
+}
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigs
+		log.Printf("\nGet signal: \"%v\"", sig)
+		done <- true
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	log.Println("Setting up IPFS node ...")
+	plugins, _, err := setupNode(ctx)
+	if err != nil {
+		log.Panicf("Failed to set up IPFS node")
+	}
+	log.Println("IPFS node is created")
+
+	<-done
+	log.Println("Close plugin")
+	err = plugins.Close()
+	if err != nil {
+		log.Panicf("Cannot close plugins: %s", err)
+	}
+}
